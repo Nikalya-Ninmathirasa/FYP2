fa15f4f5986b8eb0c22a9394fe6b7d11e179b69d
Jude Sajith <judesajith.aj@gmail.com>
2023-06-19 21:19:07 +0530

    first commit

---
+++ Home.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from pathlib import Path
+from llama_index import download_loader
+from pandasai.llm.openai import OpenAI
+
+# Initialize the AI
+llm = OpenAI()
+PandasAIReader = download_loader("PandasAIReader")
+loader = PandasAIReader(llm=llm)
+
+# Create a dropdown menu
+option = st.selectbox(
+    "Choose an option",
+    ("Price Optimization", "Search Query Analytics and Demand Forecasting", 
+    "Sentimental Analysis", "Customized and Personalized Chatbot")
+)
+
+st.subheader(option)
+
+if option in ["Price Optimization", "Sentimental Analysis", "Customized and Personalized Chatbot"]:
+    uploaded_file = st.file_uploader("Upload CSV", type=['csv'])
+
+    if uploaded_file is not None:
+        # Convert the uploaded file to a BytesIO object and read it into a pandas DataFrame
+        import io
+        import pandas as pd
+        df = pd.read_csv(io.BytesIO(uploaded_file.read()))
+
+        # Display the DataFrame
+        st.write(df)
+
+        # Get a question from the user
+        query = st.text_input("Enter your question")
+
+        if query:
+            response = loader.run_pandas_ai(df, query, is_conversational_answer=True)
+            st.write(response)
+
+elif option == "Search Query Analytics and Demand Forecasting":
+    # Input field for search query
+    search_query = st.text_input("Enter your search query")
+
+    if search_query:
+        # Placeholder for function to process the search query
+        response = process_search_query(search_query)
+        st.write(response)
