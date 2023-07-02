9fc67cbc270a75204fac4bb13926480b31b27298
Nikalya_1398 <81620144+Nikalya-Ninmathirasa@users.noreply.github.com>
2023-07-02 23:07:09 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,10 +18,7 @@ from llama_index.query_engine import RetrieverQueryEngine
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 # page heading
-st.markdown('<p style="font-size:40px; color:gray; text-align:center;">Understand the Galle Tourism Market with Guide_Bot</p> <br><br>', unsafe_allow_html=True)
-
-# Add an empty line for spacing
-
+st.markdown('<p style="font-size:40px; color:dark gray; text-align:center;">Understand the Galle Tourism Market with Guide_Bot</p> <br><br><br>', unsafe_allow_html=True)
 
 # Initialize pytrends
 pytrends = TrendReq(hl='en-US', tz=360)
